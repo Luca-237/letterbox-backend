7b8a9cd1a98221f61ad09e0efe6e9daeef127a5c
Luca-237 <luca.mmorales@gmail.com>
2025-10-29 09:41:56 -0300

    pulido
    Wrap the App component with BrowserRouter for routing.

--- letterbox-frontend/src/main.jsx
+++ letterbox-frontend/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom' // <-- 1. Importar
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Envolver App con BrowserRouter */}
+    {}
     <BrowserRouter>
       <App />
     </BrowserRouter>
