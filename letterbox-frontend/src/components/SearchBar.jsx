5008f4492b50c945ac7cf2fc6133e53197e342e0
Luca-237 <luca.mmorales@gmail.com>
2025-10-29 09:50:53 -0300

    pulido

--- letterbox-frontend/src/components/SearchBar.jsx
+++ letterbox-frontend/src/components/SearchBar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
-// Recibe una función 'onSearch' desde sus props para comunicarse con el padre
 function SearchBar({ onSearch }) {
   const [query, setQuery] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Evita que la página se recargue al enviar el formulario
+    e.preventDefault();
     if (query.trim()) {
       onSearch(query);
     }
